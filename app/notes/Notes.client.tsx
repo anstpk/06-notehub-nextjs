a8821033a1d888bc3709f2cc9e1c8a8c978822d4
anstpk <anastasiia.stupka@gmail.com>
2026-02-26 17:17:47 +0200

    fix: add props to SearchBox and implement search state

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react'; // Додаємо useState
 import { useQuery } from '@tanstack/react-query';
 import { fetchNotes } from '@/lib/api/noteApi';
 import NoteList from '@/components/NoteList/NoteList';
@@ -7,18 +8,22 @@ import SearchBox from '@/components/SearchBox/SearchBox';
 import css from './NotesPage.module.css';
 
 export default function NotesClient() {
+  // 1. Створюємо стан для пошукового рядка
+  const [searchQuery, setSearchQuery] = useState('');
+
   const { data, isLoading, error } = useQuery({
-  queryKey: ['notes'],
-  queryFn: () => fetchNotes(),
-});
+    queryKey: ['notes', searchQuery], // Додаємо searchQuery в ключ, щоб запит оновлювався
+    queryFn: () => fetchNotes(1, 12, searchQuery),
+  });
 
   if (isLoading) return <p>Loading, please wait...</p>;
   if (error) return <p>Something went wrong.</p>;
 
   return (
     <div className={css.container}>
-      <SearchBox />
-      {/* Тут буде твоя логіка створення нової нотатки */}
+      {/* 2. ПЕРЕДАЄМО ОБОВ'ЯЗКОВІ ПРОПСИ */}
+      <SearchBox value={searchQuery} onChange={setSearchQuery} />
+      
       <NoteList notes={data?.notes || []} />
     </div>
   );
